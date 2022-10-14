5383467e3bb98d40b2f3f3c86bfd4fceaf328956
DeckerLikeWolf <mallardduck27@gmail.com>
2022-10-14 18:45:33 -0400

    Update Week4_Part2.py
    Added layer to map

--- Week4_Part2.py
+++ Week4_Part2.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# ## Welcome to your notebook.
+# 
+
+# #### Run this cell to connect to your GIS and get started:
+
+# In[3]:
 
 
-import arcgis
+from arcgis.gis import GIS
+gis = GIS("home")
+
+
+# #### Now you are ready to start!
+
+# In[4]:
+
+
+map1 = gis.map('Burlington, VT')
+
+
+# In[5]:
+
+
+map1
 
 
 # In[ ]:
@@ -13,18 +33,19 @@ import arcgis
 
 
 
-# In[3]:
+# In[8]:
 
 
-from arcgis.gis import GIS
-gis = GIS()
+# Item Added From Toolbar
+# Title: VT Data - Burlington Zoning - Overlay Districts | Type: Feature Service | Owner: pbrangan
+item = gis.content.get("ab92f77ed38a4134a271049d7ad3ed58")
+item
 
 
-# In[6]:
+# In[9]:
 
 
-map1 = gis.map('Burlington, VT')
-map1
+map1.add_layer(item)
 
 
 # In[ ]:
